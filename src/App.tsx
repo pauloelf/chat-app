69623a8eeec7d203140cb16f9a3e0a17885fcc00
Paulo Sergio <contato.paulofws@gmail.com>
2024-11-24 16:53:37 -0300

    feat: add Radix Theme to application

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import '@radix-ui/themes/styles.css'
+import './App.css'
 import { useAuthState } from "react-firebase-hooks/auth"
 import { SignIn } from "./components/sign-in"
 import { auth } from "./services/firebase-config"
+import { Theme } from '@radix-ui/themes'
 
 export function App() {
   const [user] = useAuthState(auth)
 
   return (
-    <>
-      {user ? <h1>Content</h1> : <SignIn /> }
-    </>
+    <Theme accentColor='blue'>
+      {user ? <h1>Content</h1> : <SignIn />}
+    </Theme>
   )
 }
