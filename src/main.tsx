b42c72d07cfe835f601da122f856257eba2f89ca
Paulo Sergio <contato.paulofws@gmail.com>
2024-11-26 11:50:24 -0300

    fix: correct segmented-provider import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App.tsx'
 import { ThemeContextProvider } from './contexts/theme-context.tsx'
-import { SegmentedControlContextProvider } from './contexts/segment-controled-context.tsx'
+import { SegmentedControlContextProvider } from './contexts/segmented-control-context.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
