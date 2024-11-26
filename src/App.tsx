e286aa01ea5d1cf1280ce05b97039d6dd935c749
Paulo Sergio <contato.paulofws@gmail.com>
2024-11-26 11:03:21 -0300

    feat: add theme appearance

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,17 @@ import { useAuthState } from "react-firebase-hooks/auth"
 import { SignIn } from "./components/sign-in"
 import { auth } from "./services/firebase-config"
 import { Theme } from '@radix-ui/themes'
+import { NavBar } from './components/navbar/navbar'
+import { useContext } from 'react'
+import { ThemeContext } from './contexts/theme-context'
 
 export function App() {
   const [user] = useAuthState(auth)
+  const { theme } = useContext(ThemeContext)
 
   return (
-    <Theme accentColor='blue'>
-      {user ? <h1>Content</h1> : <SignIn />}
+    <Theme appearance={theme} accentColor='indigo' style={{backgroundColor: 'var(--accent-2)'}}>
+      {user ? <NavBar /> : <SignIn />}
     </Theme>
   )
 }
